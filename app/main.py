dd34d596e681693e39679b767c97293573aefbdc
alfre <alfreddinocenti@gmail.com>
2023-11-13 18:48:38 -0300

    testing Over-the-Air 1.0.6

--- app/main.py
+++ app/main.py
@@ -4,23 +4,23 @@ import senko
 
 led = Pin(2, Pin.OUT)
 
-version = "1.0.5"
+version = "1.0.6"
 OTA = senko.Senko(user="fonsecaFuentes", repo="OTA", files=["boot.py", "main.py"])
 
 while True:
     # parpadear
     led.value(1)
-    sleep(0.5)
+    sleep(0.1)
     led.value(0)
-    sleep(0.5)
+    sleep(0.2)
+    led.value(1)
+    sleep(0.3)
+    led.value(0)
+    sleep(0.4)
     led.value(1)
     sleep(0.5)
     led.value(0)
-    sleep(0.5)
-    led.value(1)
-    sleep(0.5)
-    led.value(0)
-    sleep(0.5)
+    sleep(0.6)
     if OTA.fetch():
         print("A newer version is available!")
         machine.reset()
